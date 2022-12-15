391cb22693387edf00de5cfdf6905a64bed38f21
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-12-15 10:06:32 -0600

    Firebase login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
+import { initializeApp } from 'firebase/app';
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth(firebaseApp);
 
 function App() {
+  const email = 'carlos@test.com';
+  const passwd = 'carlos123';
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, passwd);
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={login} > Log in </button>
       </header>
     </div>
   );
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDjgQ409-ubemg0s5MEbw45sZbqDAoxOlY",
+    authDomain: "test-6649b.firebaseapp.com",
+    projectId: "test-6649b",
+    storageBucket: "test-6649b.appspot.com",
+    messagingSenderId: "797913921517",
+    appId: "1:797913921517:web:ad17a735f9eea8eb70d9e4",
+    measurementId: "G-1KMC9R59JY"
+  }
